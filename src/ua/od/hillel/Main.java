365d88faff6ae8ef0bf0584c506307e550299a51
Stanislav Kvashchuk <StanislavK.1982@gmail.com>
2016-03-30 00:49:14 +0300

    Commit 3

--- src/ua/od/hillel/Main.java
+++ src/ua/od/hillel/Main.java
@@ -26,6 +26,7 @@ public class Main {
             }
         }
 
+
         System.out.println("Введите первое число число: ");
         chislo1 = scanner.nextInt();
         System.out.println("Введите второе число");
