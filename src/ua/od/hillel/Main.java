6f15c9fd8dca0201825ce79e455a60bc033756ee
Stanislav Kvashchuk <StanislavK.1982@gmail.com>
2016-03-29 22:40:59 +0300

    Add a cycle

--- src/ua/od/hillel/Main.java
+++ src/ua/od/hillel/Main.java
@@ -3,8 +3,10 @@ package ua.od.hillel;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Hello Git");
-
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
     }
 }
